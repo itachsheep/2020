9bed551eb41434888d24c6a9c363a9827bb58a1f
itachsheep <itachsheep@163.com>
2020-12-17 12:36:16 +0800

    曲棍球桌面增加颜色渐变

---
+++ openGl/GLDemo/app/src/main/java/com/tao/gldemo/airhockey2/AirHockey2Activity.java
@@ -0,0 +1,35 @@
+package com.tao.gldemo.airhockey2;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tao.gldemo.airhockey1.AirHockeyRenderer;
+
+public class AirHockey2Activity extends AppCompatActivity {
+    GLSurfaceView glSurfaceView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        glSurfaceView = new GLSurfaceView(this);
+        // Request an OpenGL ES 2.0 compatible context.
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setEGLConfigChooser(8 , 8, 8, 8, 16, 0);
+        glSurfaceView.setRenderer(new AirHockey2Renderer(this));
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //glSurfaceView.onPause();
+    }
+}
