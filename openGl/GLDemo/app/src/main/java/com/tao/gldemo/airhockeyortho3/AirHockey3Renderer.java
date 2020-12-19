66e2de99e641f301cb501d36732e914d98c0499e
itachsheep <itachsheep@163.com>
2020-12-19 18:33:09 +0800

    通过正交矩阵，调节横竖屏正常显示

--- openGl/GLDemo/app/src/main/java/com/tao/gldemo/airhockeyortho3/AirHockey3Renderer.java
+++ openGl/GLDemo/app/src/main/java/com/tao/gldemo/airhockeyortho3/AirHockey3Renderer.java
@@ -3,6 +3,7 @@ package com.tao.gldemo.airhockeyortho3;
 import android.content.Context;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import com.tao.gldemo.R;
 import com.tao.gldemo.airhockey1.ShaderHelper;
@@ -39,43 +40,47 @@ public class AirHockey3Renderer implements GLSurfaceView.Renderer {
     private final Context mContext;
 
     private int program;
-//    private static final String U_COLOR = "u_Color";
+    //    private static final String U_COLOR = "u_Color";
     private static final String A_COLOR = "a_Color";
     private static final String A_POSITION = "a_Position";
+    private static final String U_MATRIX = "u_Matrix";
 
-//    private int uColorLocation;
+    //    private int uColorLocation;
     private int aPositionLocation;
     private int aColorLocation;
+    private int uMatrixLocation;
+
+    private final float[] projectionMatrix = new float[16];
 
     public AirHockey3Renderer(Context context) {
         float[] tableVertices = {
-                0f,  0f,
+                0f, 0f,
                 0f, 14f,
                 9f, 14f,
-                9f,  0f
+                9f, 0f
         };
         float[] tableVerticesWithTriangles = {
                 // Order of coordinates: X, Y, R, G, B
                 // Triangle Fan
-                0f,    0f,   1f,   1f,   1f,//中心点
-                -0.5f, -0.5f, 1f, 0f, 0f,//左下
-                -0.3f, -0.6f, 0.7f, 0.7f, 0.7f,
-                0.2f, -0.6f, 0.7f, 0.7f, 0.7f,
+                0f, 0f, 1f, 1f, 1f,//中心点
+                -0.5f, -0.8f, 1f, 0f, 0f,//左下
+//                -0.3f, -0.6f, 0.7f, 0.7f, 0.7f,
+//                0.2f, -0.6f, 0.7f, 0.7f, 0.7f,
 
-                0.5f, -0.5f, 0f, 0f, 1f,//右下
-                0.6f, -0.3f, 0.7f, 0.7f, 0.7f,
-                0.6f, 0.3f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.8f, 0f, 0f, 1f,//右下
+//                0.6f, -0.3f, 0.7f, 0.7f, 0.7f,
+//                0.6f, 0.3f, 0.7f, 0.7f, 0.7f,
 
-                0.5f,  0.5f, 0f, 1f, 0f,//右上
-                0.2f,  0.6f, 0.7f, 0.7f, 0.7f,
-                -0.3f,  0.6f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.8f, 0f, 1f, 0f,//右上
+//                0.2f, 0.6f, 0.7f, 0.7f, 0.7f,
+//                -0.3f, 0.6f, 0.7f, 0.7f, 0.7f,
 
 
-                -0.5f,  0.5f, 0f, 0f, 1f,//左上
-                -0.6f,  0.2f, 0.7f, 0.7f, 0.7f,
-                -0.6f,  -0.2f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.8f, 0f, 0f, 1f,//左上
+//                -0.6f, 0.2f, 0.7f, 0.7f, 0.7f,
+//                -0.6f, -0.2f, 0.7f, 0.7f, 0.7f,
 
-                -0.5f, -0.5f, 1f, 0f, 0f,//左下
+                -0.5f, -0.8f, 1f, 0f, 0f,//左下
 
                 // Line 1
                 -0.5f, 0f, 1f, 0f, 0f,
@@ -83,7 +88,7 @@ public class AirHockey3Renderer implements GLSurfaceView.Renderer {
 
                 // Mallets
                 0f, -0.25f, 0f, 0f, 1f,
-                0f,  0.25f, 1f, 0f, 0f
+                0f, 0.25f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer
@@ -117,23 +122,24 @@ public class AirHockey3Renderer implements GLSurfaceView.Renderer {
         glUseProgram(program);
 
 //        uColorLocation = glGetUniformLocation(program, U_COLOR);
-        aColorLocation = glGetAttribLocation(program,A_COLOR);
-
+        aColorLocation = glGetAttribLocation(program, A_COLOR);
         aPositionLocation = glGetAttribLocation(program, A_POSITION);
+        uMatrixLocation = GLES20.glGetUniformLocation(program, U_MATRIX);
+
 
         // Bind our data, specified by the variable vertexData, to the vertex
         // attribute at location A_POSITION_LOCATION.
         vertexData.position(0);
-        glVertexAttribPointer(aPositionLocation,POSITION_COMPONENT_COUNT,GL_FLOAT,
-                false,STRIDE,vertexData);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT,
+                false, STRIDE, vertexData);
 
         glEnableVertexAttribArray(aPositionLocation);
 
         // Bind our data, specified by the variable vertexData, to the vertex
         // attribute at location A_COLOR_LOCATION.
         vertexData.position(POSITION_COMPONENT_COUNT);
-        glVertexAttribPointer(aColorLocation,COLOR_COMPONENT_COUNT,GL_FLOAT,
-                false,STRIDE,vertexData);
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT,
+                false, STRIDE, vertexData);
         glEnableVertexAttribArray(aColorLocation);
 
 
@@ -143,6 +149,18 @@ public class AirHockey3Renderer implements GLSurfaceView.Renderer {
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
         // Set the OpenGL viewport to fill the entire surface.
         GLES20.glViewport(0, 0, width, height);
+        final float aspectRatio = width > height ?
+                (float) width / (float) height :
+                (float) height / (float) width;
+        if (width > height) {
+            //横屏
+            Matrix.orthoM(projectionMatrix, 0, -aspectRatio,aspectRatio,
+                    -1f,1f,-1f,1f);
+        } else {
+            //竖屏
+            Matrix.orthoM(projectionMatrix,0,-1f,1f,
+                    -aspectRatio,aspectRatio,-1f,1f);
+        }
     }
 
     @Override
@@ -150,16 +168,21 @@ public class AirHockey3Renderer implements GLSurfaceView.Renderer {
         // Clear the rendering surface.
         glClear(GL_COLOR_BUFFER_BIT);
 
+        GLES20.glUniformMatrix4fv(uMatrixLocation,1,false,projectionMatrix,0);
         // Draw the table.
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 14);
+//        glDrawArrays(GL_TRIANGLE_FAN, 0, 14);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
 
         // Draw the center dividing line.
-        glDrawArrays(GL_LINES, 14, 2);
+//        glDrawArrays(GL_LINES, 14, 2);
+        glDrawArrays(GL_LINES, 6, 2);
 
         // Draw the first mallet.
-        glDrawArrays(GL_POINTS, 16, 1);
+//        glDrawArrays(GL_POINTS, 16, 1);
+        glDrawArrays(GL_POINTS, 8, 1);
 
         // Draw the second mallet.
-        glDrawArrays(GL_POINTS, 17, 1);
+//        glDrawArrays(GL_POINTS, 17, 1);
+        glDrawArrays(GL_POINTS, 9, 1);
     }
 }
