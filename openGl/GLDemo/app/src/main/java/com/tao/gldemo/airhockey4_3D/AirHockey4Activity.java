7ac9d998bb221ac136691185d323bbb199b46ca4
itachsheep <itachsheep@163.com>
2020-12-20 08:51:41 +0800

    曲棍球增加w分量，进入三维世界

--- openGl/GLDemo/app/src/main/java/com/tao/gldemo/airhockey4_3D/AirHockey4Activity.java
+++ openGl/GLDemo/app/src/main/java/com/tao/gldemo/airhockey4_3D/AirHockey4Activity.java
@@ -1,13 +1,13 @@
-package com.tao.gldemo.airhockeyortho3;
+package com.tao.gldemo.airhockey4_3D;
 
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.tao.gldemo.airhockey2.AirHockey2Renderer;
+import com.tao.gldemo.airhockey3ortho.AirHockey3Renderer;
 
-public class AirHockey3Activity extends AppCompatActivity {
+public class AirHockey4Activity extends AppCompatActivity {
     GLSurfaceView glSurfaceView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +17,7 @@ public class AirHockey3Activity extends AppCompatActivity {
         // Request an OpenGL ES 2.0 compatible context.
         glSurfaceView.setEGLContextClientVersion(2);
         glSurfaceView.setEGLConfigChooser(8 , 8, 8, 8, 16, 0);
-        glSurfaceView.setRenderer(new AirHockey3Renderer(this));
+        glSurfaceView.setRenderer(new AirHockey4Renderer(this));
         setContentView(glSurfaceView);
     }
 
--- openGl/GLDemo/app/src/main/java/com/tao/gldemo/airhockey4_3D/AirHockey4Renderer.java
+++ openGl/GLDemo/app/src/main/java/com/tao/gldemo/airhockey4_3D/AirHockey4Renderer.java
@@ -1,4 +1,4 @@
-package com.tao.gldemo.airhockeyortho3;
+package com.tao.gldemo.airhockey4_3D;
 
 import android.content.Context;
 import android.opengl.GLES20;
@@ -29,8 +29,8 @@ import static android.opengl.GLES20.glGetAttribLocation;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
 
-public class AirHockey3Renderer implements GLSurfaceView.Renderer {
-    private static final int POSITION_COMPONENT_COUNT = 2;
+public class AirHockey4Renderer implements GLSurfaceView.Renderer {
+    private static final int POSITION_COMPONENT_COUNT = 4;//增加w分量
     private static final int COLOR_COMPONENT_COUNT = 3;
     private static final int BYTES_PER_FLOAT = 4; //浮点数有4个字节
     private static final int STRIDE =
@@ -52,7 +52,7 @@ public class AirHockey3Renderer implements GLSurfaceView.Renderer {
 
     private final float[] projectionMatrix = new float[16];
 
-    public AirHockey3Renderer(Context context) {
+    public AirHockey4Renderer(Context context) {
         float[] tableVertices = {
                 0f, 0f,
                 0f, 14f,
@@ -60,36 +60,38 @@ public class AirHockey3Renderer implements GLSurfaceView.Renderer {
                 9f, 0f
         };
         float[] tableVerticesWithTriangles = {
-                // Order of coordinates: X, Y, R, G, B
+                // Order of coordinates: X, Y,Z,W R, G, B
                 // Triangle Fan
-                0f, 0f, 1f, 1f, 1f,//中心点
-                -0.5f, -0.8f, 1f, 0f, 0f,//左下
+                   0f,    0f,  0f, 1.5f, 1f, 1f, 1f,//中心点
+                -0.5f, -0.8f,  0f,   1f, 1f, 0f, 0f,//左下
 //                -0.3f, -0.6f, 0.7f, 0.7f, 0.7f,
 //                0.2f, -0.6f, 0.7f, 0.7f, 0.7f,
 
-                0.5f, -0.8f, 0f, 0f, 1f,//右下
+                0.5f, -0.8f,  0f,   1f, 0f, 0f, 1f,//右下
 //                0.6f, -0.3f, 0.7f, 0.7f, 0.7f,
 //                0.6f, 0.3f, 0.7f, 0.7f, 0.7f,
 
-                0.5f, 0.8f, 0f, 1f, 0f,//右上
+                0.5f,  0.8f,   0f,  2f, 0f, 1f, 0f,//右上
 //                0.2f, 0.6f, 0.7f, 0.7f, 0.7f,
 //                -0.3f, 0.6f, 0.7f, 0.7f, 0.7f,
 
 
-                -0.5f, 0.8f, 0f, 0f, 1f,//左上
+                -0.5f, 0.8f,   0f,  2f, 0f, 0f, 1f,//左上
 //                -0.6f, 0.2f, 0.7f, 0.7f, 0.7f,
 //                -0.6f, -0.2f, 0.7f, 0.7f, 0.7f,
 
-                -0.5f, -0.8f, 1f, 0f, 0f,//左下
+                -0.5f, -0.8f,  0f,  1f, 1f, 0f, 0f,//左下
 
                 // Line 1
-                -0.5f, 0f, 1f, 0f, 0f,
-                0.5f, 0f, 1f, 0f, 0f,
+                -0.5f,   0f,  0f, 1.5f, 1f, 0f, 0f,
+                0.5f,    0f,  0f, 1.5f, 1f, 0f, 0f,
 
                 // Mallets
-                0f, -0.25f, 0f, 0f, 1f,
-                0f, 0.25f, 1f, 0f, 0f
+                0f,   -0.4f,  0f, 1.25f, 0f, 0f, 1f,
+                0f,   0.25f,  0f, 1.75f, 1f, 0f, 0f
+
         };
+
 
         vertexData = ByteBuffer
                 .allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
@@ -106,9 +108,9 @@ public class AirHockey3Renderer implements GLSurfaceView.Renderer {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
         String vertexShaderSource = TextResourceReader
-                .readTextFileFromResource(mContext, R.raw.simple_vertex_shader3);
+                .readTextFileFromResource(mContext, R.raw.simple_vertex_shader4);
         String fragmentShaderSource = TextResourceReader
-                .readTextFileFromResource(mContext, R.raw.simple_fragment_shader3);
+                .readTextFileFromResource(mContext, R.raw.simple_fragment_shader4);
 
         int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
         int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
