70d575c689322063fc6f51f39ef7c85e4300a46b
itachsheep <itachsheep@163.com>
2021-01-11 09:11:04 +0800

    Xcode opengl 01-triangle

---
+++ openGl/XCode-opengl/01-triangle/01-triangle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  01-triangle
+//
+//  Created by wei tao on 2021/1/11.
+//  Copyright © 2021 wei tao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
